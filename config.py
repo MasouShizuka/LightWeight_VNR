e9bc213a62cf551c656b3ebc461e447423c4e6f8
MasouShizuka <qwe569746537@qq.com>
2021-09-25 15:48:07 +0800

    update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ default_config = {
     'copy': False,
 
     # 游戏相关
-    'locale_emulator_path': os.path.abspath('.') + r'\Locale Emulator',
+    'locale_emulator_path': os.path.join(os.path.abspath('.'), 'Locale Emulator'),
 
     # Textractor相关
-    'textractor_path': os.path.abspath('.') + r'\Textractor',
+    'textractor_path': os.path.join(os.path.abspath('.'), 'Textractor'),
     'textractor_interval': 0,
 
     # OCR相关
-    'tesseract_OCR_path': os.path.abspath('.') + r'Tesseract-OCR',
+    'tesseract_OCR_path': os.path.join(os.path.abspath('.'), 'Tesseract-OCR'),
     'OCR_language': '日文',
     'OCR_interval': 1,
     'threshold_way': 'BINARY',
@@ -22,9 +22,9 @@ default_config = {
 
     # 翻译相关
     'jbeijing': False,
-    'jbeijing_path': os.path.abspath('.') + r'\jbeijing',
+    'jbeijing_path': os.path.join(os.path.abspath('.'), 'Jbeijing'),
     'jbeijing_to': '简体中文',
-    'youdao_path': os.path.abspath('.') + r'\有道词典',
+    'youdao_path': os.path.join(os.path.abspath('.'), '有道词典'),
     'youdao_get_translate': True,
     'youdao_interval': 1.0,
     'baidu': False,
@@ -32,21 +32,21 @@ default_config = {
     'baidu_key': '',
 
     # TTS相关
-    'yukari_path': os.path.abspath('.') + r'\Yukari',
+    'yukari_path': os.path.join(os.path.abspath('.'), 'Yukari'),
     'yukari_constantly': False,
     'yukari_aside': True,
     'yukari_character': True,
-    'tamiyasu_path': os.path.abspath('.') + r'\Tamiyasu',
+    'tamiyasu_path': os.path.join(os.path.abspath('.'), 'Tamiyasu'),
     'tamiyasu_constantly': False,
     'tamiyasu_aside': True,
     'tamiyasu_character': True,
 
     # 文本相关
     'deduplication': 1,
-    'deduplication_auto': False,
+    'deduplication_auto': True,
     'garbage_chars': '',
     're': '',
 
     # 浮动窗口相关
-    'floating_text_original': True,
+    'floating_text_original': False,
 }
